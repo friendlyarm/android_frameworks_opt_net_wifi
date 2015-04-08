92477a7050495771d4b409833afdb1afd07d8f14
Lawrence Mok <lawrence.mok@amlogic.com>
2015-04-08 15:47:16 -0700

    quiet wifi scan log messages
    Change-Id: I5de63a6e9973efb17128165c55ad02d33895b46a

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -6882,9 +6882,9 @@ public class WifiStateMachine extends StateMachine {
                 freqs.append(channel.toString());
                 first = false;
             }
-            //if (DBG) {
-            loge("WifiStateMachine starting scan for " + config.configKey() + " with " + freqs);
-            //}
+            if (DBG) {
+                loge("WifiStateMachine starting scan for " + config.configKey() + " with " + freqs);
+            }
             // Call wifi native to start the scan
             if (startScanNative(
                     WifiNative.SCAN_WITHOUT_CONNECTION_SETUP,
@@ -7037,13 +7037,13 @@ public class WifiStateMachine extends StateMachine {
                     deferMessage(message);
                     break;
                 case CMD_START_SCAN:
-                    //if (DBG) {
+                    if (DBG) {
                         loge("WifiStateMachine CMD_START_SCAN source " + message.arg1
                               + " txSuccessRate="+String.format( "%.2f", mWifiInfo.txSuccessRate)
                               + " rxSuccessRate="+String.format( "%.2f", mWifiInfo.rxSuccessRate)
                               + " targetRoamBSSID=" + mTargetRoamBSSID
                               + " RSSI=" + mWifiInfo.getRssi());
-                    //}
+                    }
                     if (message.arg1 == SCAN_ALARM_SOURCE) {
                         // Check if the CMD_START_SCAN message is obsolete (and thus if it should
                         // not be processed) and restart the scan if needed
